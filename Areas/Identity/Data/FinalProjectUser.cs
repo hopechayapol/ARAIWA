4e9580cb3affdd700198dc2160aa0bf4b6e4e25a
Padthaixstillworking <hope.chayapol@gmail.com>
2024-11-19 15:11:04 +0700

    Initial commit

--- Areas/Identity/Data/FinalProjectUser.cs
+++ Areas/Identity/Data/FinalProjectUser.cs
@@ -12,6 +12,5 @@ public class FinalProjectUser : IdentityUser
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string MobilePhone { get; set; }
-
 }
 
